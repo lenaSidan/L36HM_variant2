befe0a7d15ec964e4df90eb9f2ad938e8f1d1b6e
Elena Sidanova <l.sidan@gmx.de>
2023-05-07 21:40:31 +0200

    add new directory res

--- src/Main.java
+++ src/Main.java
@@ -3,19 +3,21 @@ import java.util.List;
 
 public class Main {
     public static final String PASSENGER_LIST = "./res/passengerList";
+    //Jack Peter,Bob Snow,Anna May,Bobby Klaus,Maria Dorris
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter writer = new BufferedWriter(new FileWriter(PASSENGER_LIST));
-        try {
+        BufferedWriter writer = new BufferedWriter(new FileWriter(PASSENGER_LIST,false));
+
             Ship allureOfTheSeas = new Ship("Allure of the Seas", 4, 22, "Капитан Грей");
             allureOfTheSeas.getCapacity();
             System.out.println("Введите имена пассажиров (через запятую): ");
-            //Jack Peter,Bob Snow,Anna May,Bobby Klaus,Maria Dorris
             String[] names = reader.readLine().split(",");
 
             for (String name : names) {
                 Passenger passenger = new Passenger(name);
                 allureOfTheSeas.pickPassenger(passenger);
+                writer.write(passenger.getName());
+                writer.newLine();
             }
             System.out.println("Введите имя пассажира, что бы проверить его по списку: ");
             String name = reader.readLine();
@@ -25,9 +27,6 @@ public class Main {
                 System.out.println("Пассажир " + name + " в списке не значится.");
                 writer.write(reader.readLine());
             }
-        } catch (IOException e){
-            e.printStackTrace();
-        }
         reader.close();
         writer.close();
     }
