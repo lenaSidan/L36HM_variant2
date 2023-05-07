b4d0a99294aa0ab310ee6a1204d5f91ad88c5f99
Elena Sidanova <l.sidan@gmx.de>
2023-05-07 17:51:09 +0200

    add BufferedReader and BufferedWriter

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,34 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.List;
 
 public class Main {
+    public static final String PASSENGER_LIST = "./res/passengerList";
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        Ship allureOfTheSeas = new Ship("Allure of the Seas", 4, 22,"Капитан Грей");
-        allureOfTheSeas.getCapacity();
-        System.out.println("Введите имена пассажиров (через запятую): ");
-        //Jack Peter,Bob Snow,Anna May,Bobby Klaus,Maria Dorris
-        String[]names = reader.readLine().split(",");
-            for (String name : names){
-              Passenger passenger = new Passenger(name);
-              allureOfTheSeas.pickPassenger(passenger);
-        }
-        System.out.println("Введите имя пассажира, что бы проверить его по списку: ");
-            String name = reader.readLine();
-            if(allureOfTheSeas.dropPassenger(name)){
-            System.out.println("Пассажир " +name+ " Ваша высадка.");
-            }else {
-                System.out.println("Пассажир " +name+ " в списке не значится.");
+        BufferedWriter writer = new BufferedWriter(new FileWriter(PASSENGER_LIST));
+        try {
+            Ship allureOfTheSeas = new Ship("Allure of the Seas", 4, 22, "Капитан Грей");
+            allureOfTheSeas.getCapacity();
+            System.out.println("Введите имена пассажиров (через запятую): ");
+            //Jack Peter,Bob Snow,Anna May,Bobby Klaus,Maria Dorris
+            String[] names = reader.readLine().split(",");
+
+            for (String name : names) {
+                Passenger passenger = new Passenger(name);
+                allureOfTheSeas.pickPassenger(passenger);
             }
+            System.out.println("Введите имя пассажира, что бы проверить его по списку: ");
+            String name = reader.readLine();
+            if (allureOfTheSeas.dropPassenger(name)) {
+                System.out.println("Пассажир " + name + " Ваша высадка.");
+            } else {
+                System.out.println("Пассажир " + name + " в списке не значится.");
+                writer.write(reader.readLine());
+            }
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+        reader.close();
+        writer.close();
     }
 }
